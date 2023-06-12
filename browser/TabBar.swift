eb7ede167234f5440f9a031cef0f29a83495fef9
levaniii <mr.charuashvili@gmail.com>
2023-06-12 19:48:39 +0400

    back and forward webkit features added along with search

--- browser/TabBar.swift
+++ browser/TabBar.swift
@@ -6,31 +6,50 @@
 //
 
 import UIKit
+import WebKit
 
-class TabBar: UITabBarController {
+protocol TabBarViewControllerDelegate: AnyObject {
+    func goBack()
+    func textFieldDidReturn(withText text: String)
+    func goForward()
+}
+class TabBar: UITabBarController, UITabBarControllerDelegate,UITextFieldDelegate {
     
     private let textField = UITextField()
     private var textFieldBottomConstraint: NSLayoutConstraint!
     private var keyboardHeight: CGFloat = 0.0
+    weak var tabBarDelegate: TabBarViewControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Set the delegate of the tab bar controller to self
+        self.delegate = self
+        textField.delegate = self
+        
         // Create view controllers for each tab
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let firstViewController = storyboard.instantiateViewController(withIdentifier: "ViewController")
-        firstViewController.tabBarItem = UITabBarItem(title: "Tab 1", image: UIImage(systemName: "1.circle"), tag: 0)
+        firstViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "arrow.left"), tag: 0)
         
         let secondViewController = UIViewController()
         secondViewController.view.backgroundColor = .green
-        secondViewController.tabBarItem = UITabBarItem(title: "Tab 2", image: UIImage(systemName: "2.circle"), tag: 1)
+        secondViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "arrow.right"), tag: 1)
         
         let thirdViewController = UIViewController()
         thirdViewController.view.backgroundColor = .blue
         thirdViewController.tabBarItem = UITabBarItem(title: "Tab 3", image: UIImage(systemName: "3.circle"), tag: 2)
         
+        let fourthViewController = UIViewController()
+        fourthViewController.view.backgroundColor = .blue
+        fourthViewController.tabBarItem = UITabBarItem(title: "Tab 4", image: UIImage(systemName: "4.circle"), tag: 3)
+        
+        let fifthViewController = UIViewController()
+        fifthViewController.view.backgroundColor = .blue
+        fifthViewController.tabBarItem = UITabBarItem(title: "Tab 5", image: UIImage(systemName: "5.circle"), tag: 4)
+        
         // Set the view controllers for the tab bar
-        self.viewControllers = [firstViewController, secondViewController, thirdViewController]
+        self.viewControllers = [firstViewController, secondViewController, thirdViewController,fourthViewController,fifthViewController]
         
         // Add the text field as a subview of the tab bar controller's view
         textField.borderStyle = .roundedRect
@@ -38,15 +57,17 @@ class TabBar: UITabBarController {
         textField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(textField)
            
-           // Configure constraints for the text field
-        let textFieldBottomConstraint = textField.bottomAnchor.constraint(equalTo: tabBar.topAnchor, constant: -8)
+        // Configure constraints for the text field
+        textFieldBottomConstraint = textField.bottomAnchor.constraint(equalTo: tabBar.topAnchor, constant: -8)
         textFieldBottomConstraint.isActive = true
         NSLayoutConstraint.activate([
             textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            textField.heightAnchor.constraint(equalToConstant: 30),
             textFieldBottomConstraint
-
         ])
+        
+        // Register for keyboard notifications
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
@@ -67,14 +88,46 @@ class TabBar: UITabBarController {
         moveTextFieldWithKeyboard()
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text {
+            tabBarDelegate?.textFieldDidReturn(withText: text)
+        }
+        return true
+    }
+    
     private func moveTextFieldWithKeyboard() {
         let safeAreaBottom = view.safeAreaInsets.bottom
         let tabBarHeight = tabBar.frame.height
-        let textFieldBottomMargin: CGFloat = 8.0
+        let textFieldBottomMargin: CGFloat = 2.0
         
         let keyboardOffset = keyboardHeight - safeAreaBottom + tabBarHeight + textFieldBottomMargin
         
         textField.transform = CGAffineTransform(translationX: 0, y: -keyboardOffset)
     }
-
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 0 {
+            // Perform custom action for Tab 1
+            performTab1Action()
+            return false // Return false to prevent switching tabs
+        } else if viewController.tabBarItem.tag == 1 {
+            // Perform custom action for Tab 2
+            performTab2Action()
+            return false // Return false to prevent switching tabs
+        }
+        
+        return true // Allow switching to other tabs by default
+    }
+    
+    private func performTab1Action() {
+        print("go back form page")
+        tabBarDelegate?.goBack()
+        // Add your custom code here
+    }
+    
+    private func performTab2Action() {
+        print("go forward")
+        tabBarDelegate?.goForward()
+        // Add your custom code here
+    }
 }
--- browser/ViewController.swift
+++ browser/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKNavigationDelegate,TabBarViewControllerDelegate {
 
+    var webView:WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView(frame: view.bounds)
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .brown
+        
+        let url = URL(string: "https://roblox.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        if let tabBarController = self.tabBarController as? TabBar {
+            tabBarController.tabBarDelegate = self
+        }
     }
-
+    
+    
+    
+    // delegates from tabbar below
+    
+    func textFieldDidReturn(withText text: String) {
+        var newUrl = URL(string: "\(text)")
+        webView.load(URLRequest(url: newUrl!))
+        }
+    
+    func goBack() {
+        if webView.canGoBack{
+            webView.goBack()
+        } else {
+            print("cant go back")
+        }
+    }
+    
+    func goForward() {
+        if webView.canGoForward {
+            webView.goForward()
+        } else {
+            print("cant go forward")
+        }
+    }
 
 }
 
